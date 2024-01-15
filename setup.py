969dcbbddc76f9ad315d4006963a4bc30092fc61
Peter Donaghey <peter.donaghey@point-topic.com>
2024-01-15 15:18:22 +0000

    changed root dir name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='loading_bar',
+    name='simple_loading_bar_utility',
     version='0.1',
     packages=find_packages(),
     description='A simple loading bar utility',
